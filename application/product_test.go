afa7d6d94c321eddc69ff4209bdca95ce4517038
Mateus Deitos <mate.deitos@hotmail.com>
2024-02-01 21:28:04 -0300

    feat: testing isValid, Enable and Disable methods

--- application/product_test.go
+++ application/product_test.go
@@ -4,16 +4,63 @@ import (
 	"testing"
 
 	"github.com/mateusdeitos/golang-hexagonal/application"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 )
 
 func TestProduct_Enable(t *testing.T) {
-	product := application.Product{}
-	product.Name = "Hello"
-	product.Status = application.DISABLED
-	product.Price = 10
+	product := application.Product{
+		Name: "Hello",
+		Price: 10,
+		Status: application.DISABLED,
+	}
 
 	err := product.Enable()
 
 	require.Nil(t, err)
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{
+		Name: "Hello",
+		Price: 0,
+		Status: application.ENABLED,
+	}
+
+	err := product.Disable()
+
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "the price must be 0 to disable the product", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{
+		ID: uuid.NewV4().String(),
+		Name: "Hello",
+		Price: 10,
+		Status: application.DISABLED,
+	}
+
+	isValid, err := product.IsValid()
+
+	require.Nil(t, err)
+	require.True(t, isValid)
+	
+	product.Status = "INVALID"
+	isValid, err = product.IsValid()
+	require.False(t, isValid)
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	isValid, err = product.IsValid()
+	require.True(t, isValid)
+	require.Nil(t, err)
+
+	product.Price = -10
+	isValid, err = product.IsValid()
+	require.False(t, isValid)
+	require.Equal(t, "the price must be greater or equal than 0", err.Error())
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/mateusdeitos/golang-hexagonal
 
 go 1.16
 
-require github.com/stretchr/testify v1.8.0
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
+	github.com/dgryski/trifles v0.0.0-20230903005119-f50d829f2e54
+	github.com/satori/go.uuid v1.2.0
+	github.com/stretchr/testify v1.8.0
+)
