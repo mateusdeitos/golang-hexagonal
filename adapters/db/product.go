32f71dc0cb8556a13895967b8dee4502e54f97a7
mateusdeitos <mate.deitos@hotmail.com>
2024-03-13 20:25:51 -0300

    feat: create/update and Save

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -32,3 +32,51 @@ func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
 
 	return &product, nil
 }
+
+func (p *ProductDb) Save(product application.ProductInterface) (application.ProductInterface, error) {
+	var count int
+	p.db.QueryRow("SELECT COUNT(*) FROM products WHERE id = ?", product.GetID()).Scan(&count)
+	if count == 0 {
+		_, err := p.create(product)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		_, err := p.update(product)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return product, nil
+}
+
+func (p *ProductDb) create(product application.ProductInterface) (application.ProductInterface, error) {
+	stmt, err := p.db.Prepare("INSERT INTO products (id, name, price, status) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = stmt.Exec(product.GetID(), product.GetName(), product.GetPrice(), product.GetStatus())
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (p *ProductDb) update(product application.ProductInterface) (application.ProductInterface, error) {
+	_, err := p.db.Exec(
+		"UPDATE products SET name = ?, price = ?, status = ? WHERE id = ?",
+		product.GetName(),
+		product.GetPrice(),
+		product.GetStatus(),
+		product.GetID(),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
